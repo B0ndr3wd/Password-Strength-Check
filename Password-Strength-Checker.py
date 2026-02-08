dd716755252f4be129f4ae22dcd8b909b20c5daf
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-08 09:28:59 +0100

    Added r'' in other lines

--- Password-Strength-Checker.py
+++ Password-Strength-Checker.py
@@ -3,12 +3,12 @@ import re
 def password_strength():
     '''Check ur Password'''
     password = input("Enter a password : ")
-    if len(password) < 9:
+    if len(password) < 8:
         print("Bad Password --> Add more characters")
     else:
         x = re.findall(r"[~`!@#$%^&*()_\-+=\[\]{}|]", password)
-        y = re.findall("[0123456789]", password)
-        z = re.findall("[a-zA-Z]", password)
+        y = re.findall(r"[0123456789]", password)
+        z = re.findall(r"[a-zA-Z]", password)
         if x and y and z:
             print("Strong Password")
         elif (x and y) or (y and z) or (z and x):
